ffc7099816670bdfec9e726fe619ffa7a0b63919
Vladyslav Molibozhenko <vlm@sitecore.net>
2017-10-18 13:00:34 +0300

    initial commit

---
+++ src/Sitecore.Support.123058.136080/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.123058.136080")]
+[assembly: AssemblyProduct("Sitecore.Support.123058.136080")]
+[assembly: ComVisible(false)]
